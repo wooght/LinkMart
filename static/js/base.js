fd6cb5a575f6ed6f93f003bb34c3e976d9da44ab
wooght <wooght@126.com>
2020-11-03 02:38:30 +0800

    修改分类概况2

--- static/js/base.js
+++ static/js/base.js
@@ -16,3 +16,25 @@ function are_ok(name,str_code,id){
         get_change_result(str_code,id)
     }
 }
+//数组求和函数
+function array_sum(arr,sk1,sk2){
+    sum=0
+    for(i=sk1,j=sk2;i<sk2;i++){
+        sum+=arr[i]
+    }
+    return sum;
+}
+// 数组求平均值 并返回数组
+function array_average(arr){
+    average_arr = []
+    for(i=0;i<arr.length;i++){
+       if(i<30){
+            average_arr.push(arr[i])
+       }else{
+            average_arr.push(array_sum(arr,i-30,i)/30)
+       }
+    }
+    return average_arr;
+}
+arr=[1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4]
+alert(array_average(arr))
